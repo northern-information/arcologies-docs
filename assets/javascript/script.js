39dc005f053cbbd10422033a99b4dfe5cc8c1476
Tyler Etters <tyler@etters.co>
2020-08-17 05:30:13 -0500

    ++

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,1 @@
 $('body').scrollspy({ target: '#toc', offset: 150 });
-// $(".jumbotron").css({ height: $(window).height() + "px" });
-
-// $(window).on("resize", function() {
-//   $(".jumbotron").css({ height: $(window).height() + "px" });
-// });
